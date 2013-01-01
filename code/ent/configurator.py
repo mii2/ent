6f91d78148fb1865c101fb79f6986e835298f440
mi2 <a@b.c>
2013-01-02 00:34:51 +0600

    Первый хоть что-то выводящий коммит ConfigObj for config parsing

---
+++ code/ent/configurator.py
@@ -0,0 +1,9 @@
+﻿# -*- coding: utf-8 -*-
+
+from configobj import ConfigObj
+
+class Configurator:
+    def __init__(self, filename=u'../config/main.cfg'):
+        '''The main configuration. Busines-logic do not use this class. It use config-files, but logic use DB'''
+        self._filename = filename
+        self.parser = ConfigObj(self._filename, default_encoding='utf-8', encoding='utf-8')
---
+++ code/index.py
@@ -0,0 +1,13 @@
+﻿# -*- coding: utf-8 -*-
+
+from bottle import run, route, template
+import ent
+
+config = ent.Configurator()
+
+@route('/')
+def root_dir():
+    return unicode(config.parser['sqlite']).decode('unicode-escape')
+    #return template('../templ/index.tpl')
+    
+run(reloader=True)
